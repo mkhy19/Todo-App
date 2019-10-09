8b9fc5be26541ea1b702b8ee0db67e9276ad947e
mkhy19 <mkhy88519@gmail.com>
2019-10-10 00:06:49 +0200

    Add AddItem component

---
+++ src/Components/AddItem/AddItem.js
@@ -0,0 +1,49 @@
+import React , {Component} from 'react';
+
+import './AddItem.css';
+
+class AddItem extends Component{
+    state = {
+        name: '',
+        age: ''
+    }
+
+    handleChange = (ev) => {
+        this.setState({
+            [ev.target.id] : ev.target.value
+            
+        })
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+
+        //Handle validation
+        if(ev.target.name.value === '' || ev.target.age.value === ''){
+            return false
+        } else{
+            this.props.addNewItem(this.state)
+
+            this.setState({
+                name: '',
+                age: ''
+            })
+
+        }
+
+    }
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Enter name.." id="name" onChange={this.handleChange} value={this.state.name} />
+                    <input type="number" placeholder="Enter age.." id="age" onChange={this.handleChange} value={this.state.age}/>
+                    <input type="submit" value="add" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddItem;
